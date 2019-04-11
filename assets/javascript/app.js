f215a994e0d75e56be7fbd248645e1ed3f2d13bf
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-04-10 23:21:09 -0700

    added questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,187 @@
-// Trivia Questions
+$(document).ready(function () {
 
-var quest = [
-    1, 'Through what part of the body do dogs sweat?',
+    // start game function 
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click', '.option', trivia.guessChecker);
 
+})
 
+// Track variables 
 
+var trivia = {
 
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 10,
+    timerOn: false,
+    timerId: '',
 
+    // questions options and answers 
+    questions: {
+        q1: 'Normal adult dogs have how many teeth?',
+        q2: 'Through what part of the body do dogs sweat?',
+        q3: 'What is the most common training command taught to dogs?',
+        q4: 'What is a dogs most highly developed sense?',
+        q5: 'Which dog breed is the smallest of them all?',
+        q6: 'Which dog breed has a black tongue?',
+        q7: 'What is the most popular breed of dog, according to the American Kennel Clubs registration?',
+    },
+    options: {
+        q1: ['24', '38', '42', '32'],
+        q2: ['Mouth', 'Ears', 'Nose', 'Paws'],
+        q3: ['Stay', 'Beg', 'Sit', 'Dance'],
+        q4: ['Taste', 'Smell', 'Sight', 'Touch'],
+        q5: ['Dachshund', 'Shih Tzu', 'Pomeranian', 'Chihuahua'],
+        q6: ['Husky', 'Labrador', 'Weimaraner', 'Chow Chow'],
+        q7: ['Golden Retriever', 'Beagle', 'German Shepherd', 'Labrador']
+    },
+    answers: {
+        q1: '42',
+        q2: 'Paws',
+        q3: 'Sit',
+        q4: 'Smell',
+        q5: 'Chihuahua',
+        q6: 'Chow Chow',
+        q7: 'Labrador'
 
+// trivia methods
+  // method to initialize game
+  startGame: function () {
+            // restarting game results
+            trivia.currentSet = 0;
+            trivia.correct = 0;
+            trivia.incorrect = 0;
+            trivia.unanswered = 0;
+            clearInterval(trivia.timerId);
 
+            // show game section
+            $('#game').show();
 
+            //  empty last results
+            $('#results').html('');
 
-// Start button
-// Timer count down
-// Answer selections
-// Track correct answers
-// Track incorrect answers
-//Track unanswered questions
+            // show timer
+            $('#timer').text(trivia.timer);
 
-// Functions
+            // remove start button
+            $('#start').hide();
+
+            $('#remaining-time').show();
+
+            // ask first question
+            trivia.nextQuestion();
+
+        },
+        // method to loop through and display questions and options 
+        nextQuestion: function () {
+
+            // set timer to 20 seconds each question
+            trivia.timer = 10;
+            $('#timer').removeClass('last-seconds');
+            $('#timer').text(trivia.timer);
+
+            // to prevent timer speed up
+            if (!trivia.timerOn) {
+                trivia.timerId = setInterval(trivia.timerRunning, 1000);
+            }
+
+            // gets all the questions then indexes the current questions
+            var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+            $('#question').text(questionContent);
+
+            // an array of all the user options for the current question
+            var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+
+            // creates all the trivia guess options in the html
+            $.each(questionOptions, function (index, key) {
+                $('#options').append($('<button class="option btn btn-info btn-lg">' + key + '</button>'));
+            })
+
+        },
+        // method to decrement counter and count unanswered if timer runs out
+        timerRunning: function () {
+            // if timer still has time left and there are still questions left to ask
+            if (trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length) {
+                $('#timer').text(trivia.timer);
+                trivia.timer--;
+                if (trivia.timer === 4) {
+                    $('#timer').addClass('last-seconds');
+                }
+            }
+            // the time has run out and increment unanswered, run result
+            else if (trivia.timer === -1) {
+                trivia.unanswered++;
+                trivia.result = false;
+                clearInterval(trivia.timerId);
+                resultId = setTimeout(trivia.guessResult, 1000);
+                $('#results').html('<h3>Out of time! The answer was ' + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
+            }
+            // if all the questions have been shown end the game, show results
+            else if (trivia.currentSet === Object.keys(trivia.questions).length) {
+
+                // adds results of game (correct, incorrect, unanswered) to the page
+                $('#results')
+                    .html('<h3>Thank you for playing!</h3>' +
+                        '<p>Correct: ' + trivia.correct + '</p>' +
+                        '<p>Incorrect: ' + trivia.incorrect + '</p>' +
+                        '<p>Unaswered: ' + trivia.unanswered + '</p>' +
+                        '<p>Please play again!</p>');
+
+                // hide game sction
+                $('#game').hide();
+
+                // show start button to begin a new game
+                $('#start').show();
+            }
+
+        },
+        // method to evaluate the option clicked
+        guessChecker: function () {
+
+            // timer ID for gameResult setTimeout
+            var resultId;
+
+            // the answer to the current question being asked
+            var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+
+            // if the text of the option picked matches the answer of the current question, increment correct
+            if ($(this).text() === currentAnswer) {
+                // turn button green for correct
+                $(this).addClass('btn-success').removeClass('btn-info');
+
+                trivia.correct++;
+                clearInterval(trivia.timerId);
+                resultId = setTimeout(trivia.guessResult, 1000);
+                $('#results').html('<h3>Correct Answer!</h3>');
+            }
+            // else the user picked the wrong option, increment incorrect
+            else {
+                // turn button clicked red for incorrect
+                $(this).addClass('btn-danger').removeClass('btn-info');
+
+                trivia.incorrect++;
+                clearInterval(trivia.timerId);
+                resultId = setTimeout(trivia.guessResult, 1000);
+                $('#results').html('<h3>Better luck next time! ' + currentAnswer + '</h3>');
+            }
+
+        },
+        // method to remove previous question results and options
+        guessResult: function () {
+
+            // increment to next question set
+            trivia.currentSet++;
+
+            // remove the options and results
+            $('.option').remove();
+            $('#results h3').remove();
+
+            // begin next question
+            trivia.nextQuestion();
+
+        }
+
+    }
